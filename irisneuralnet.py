9c065bd94dba686e2ec5b5f99860817593d8f0b7
mynameisjanus <janus_advincula@hotmail.com>
2020-03-07 01:41:13 +0800

    updates

---
+++ irisneuralnet.py
@@ -0,0 +1,68 @@
+# Neural Network
+
+import numpy as np
+
+def relu(x):
+        return np.maximum(0, x)
+
+def drelu(x):
+        return 1 * (x > 0)
+
+def output_layer_activation(x):
+        return x
+
+def output_layer_activation_derivative(x):
+        return 1
+        
+class NeuralNet():
+        
+        def __init__(self, X_train, y_train, hidden_layer, lr, epochs):
+                self.W1 = np.ones((hidden_layer, X_train.shape[1]))
+                self.W2 = np.ones((1, hidden_layer))
+                self.b = np.zeros((hidden_layer, 1))
+                self.learning_rate = lr
+                self.epochs = epochs
+                self.training_points = X_train
+                self.ypoints = y_train
+                
+        def train(self, X, y):
+                f1 = np.vectorize(relu)
+                df1 = np.vectorize(drelu)
+                
+                z1 = self.W1 @ X + self.b
+                a1 = f1(z1)
+                z2 = self.W2 @ a1
+                activated_output = output_layer_activation(z2)
+                
+                output_layer_error = (activated_output - y) * output_layer_activation_derivative(z2)
+                
+                hidden_layer_error = np.multiply((self.W2).T @ output_layer_error, df1(z1))
+                
+                b_grad = hidden_layer_error
+                W2_grad = (a1 * output_layer_error).T
+                W1_grad = (X * hidden_layer_error.T).T
+                
+                self.b = self.b - self.learning_rate * b_grad
+                self.W1 = self.W1 - self.learning_rate * W1_grad
+                self.W2 = self.W2 - self.learning_rate * W2_grad
+                
+        def predict(self, X_test):
+                f1 = np.vectorize(relu)
+                
+                z1 = self.W1 @ X_test + self.b
+                a1 = f1(z1)
+                z2 = self.W2 @ a1
+                activated_output = output_layer_activation(z2)
+                
+                return activated_output.item()
+        
+        def train_neural_network(self):
+                for epoch in range(self.epochs):
+                        for x, y in zip(self.training_points, self.ypoints):
+                                self.train(x, y)
+        
+        def test_neural_network(self, X_test, y_test):
+                y_pred = []
+                for point in X_test:
+                        y_pred.append(self.predict(point))
+                return np.mean(y_pred == y_test)
--- main.py
+++ main.py
@@ -1,1 +1,27 @@
+import numpy as np
+import irisneuralnet as IRN
+
 # Download the dataset
+from sklearn.datasets import load_iris
+iris = load_iris()
+X, y = iris.data, iris.target
+
+# Split the data into training and test sets
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = 0)
+
+# Run kNN
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors = 1)
+knn.fit(X_train, y_train)
+
+y_pred = knn.predict(X_test)
+
+# Run Neural Network
+x = IRN.NeuralNet(X_train, y_train, hidden_layer = 10, lr = 0.001, epochs = 5)
+x.train_neural_network()
+x.test_neural_network(X_test, y_test)
+
+# Compare errors
+print("kNN test score: {:.4f}".format(np.mean(y_pred == y_test)))
+print("Neural Network test score:")
